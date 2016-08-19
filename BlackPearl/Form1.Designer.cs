3a643e683deb5138400dea5a39393624f0939bc4
stefancvetkovic <stefan.cv5@gmail.com>
2016-08-19 15:58:38 +0200

    final v1

---
+++ BlackPearl/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace BlackPearl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPath = new System.Windows.Forms.Button();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.txtReplace = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDestinationJob = new System.Windows.Forms.TextBox();
+            this.txtSourceJob = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnExtract = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnPath
+            // 
+            this.btnPath.Location = new System.Drawing.Point(268, 72);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(96, 37);
+            this.btnPath.TabIndex = 2;
+            this.btnPath.Text = "Izvor";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(268, 120);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(96, 37);
+            this.btnReplace.TabIndex = 5;
+            this.btnReplace.Text = "Zameni!";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(268, 35);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(234, 26);
+            this.txtPath.TabIndex = 1;
+            this.txtPath.Text = "D:\\New folder\\";
+            this.txtPath.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtFind
+            // 
+            this.txtFind.Location = new System.Drawing.Point(8, 35);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(234, 26);
+            this.txtFind.TabIndex = 3;
+            // 
+            // txtReplace
+            // 
+            this.txtReplace.Location = new System.Drawing.Point(8, 120);
+            this.txtReplace.Name = "txtReplace";
+            this.txtReplace.Size = new System.Drawing.Size(234, 26);
+            this.txtReplace.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(264, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Izaberite folder";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 12);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Pronađi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 97);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Zameni sa";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(18, 18);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(747, 240);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txtFind);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.txtPath);
+            this.tabPage1.Controls.Add(this.btnReplace);
+            this.tabPage1.Controls.Add(this.btnPath);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.txtReplace);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPage1.Size = new System.Drawing.Size(739, 207);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Zamena";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.txtDestinationJob);
+            this.tabPage2.Controls.Add(this.txtSourceJob);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.btnExtract);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPage2.Size = new System.Drawing.Size(739, 207);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Opis poslova";
+            this.tabPage2.ToolTipText = "rwrweww";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(450, 5);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Sačuvaj u:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 5);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(406, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Izaberite iz kog foldera uzimate u obzir word dokumenta:";
+            // 
+            // txtDestinationJob
+            // 
+            this.txtDestinationJob.Location = new System.Drawing.Point(454, 28);
+            this.txtDestinationJob.Name = "txtDestinationJob";
+            this.txtDestinationJob.Size = new System.Drawing.Size(234, 26);
+            this.txtDestinationJob.TabIndex = 7;
+            // 
+            // txtSourceJob
+            // 
+            this.txtSourceJob.Location = new System.Drawing.Point(14, 28);
+            this.txtSourceJob.Name = "txtSourceJob";
+            this.txtSourceJob.Size = new System.Drawing.Size(234, 26);
+            this.txtSourceJob.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(694, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(51, 37);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(254, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 37);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnExtract
+            // 
+            this.btnExtract.Location = new System.Drawing.Point(290, 122);
+            this.btnExtract.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnExtract.Name = "btnExtract";
+            this.btnExtract.Size = new System.Drawing.Size(126, 54);
+            this.btnExtract.TabIndex = 0;
+            this.btnExtract.Text = "Kreiraj txt fajlove";
+            this.btnExtract.UseVisualStyleBackColor = true;
+            this.btnExtract.Click += new System.EventHandler(this.btnExtract_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(774, 52);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(656, 646);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1506, 772);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.tabControl1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Black Pearl";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.TextBox txtReplace;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnExtract;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSourceJob;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDestinationJob;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ BlackPearl/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+//using Application = Microsoft.Office.Interop.Word.System.Windows.Forms.Application;
+
+namespace BlackPearl
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            DialogResult result = fbd.ShowDialog();
+
+            if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                txtPath.Text = fbd.SelectedPath;
+                string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+                MessageBox.Show("Pronađeno fajlova: " + files.Length.ToString(), "Message");
+
+            }
+            
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+           try
+           {
+               if (txtPath.Text == "" || txtFind.Text == "" || txtReplace.Text == "")
+               {
+                   DialogResult result = MessageBox.Show("Morate popuniti sva polja", "UPOZORENJE",
+                       MessageBoxButtons.RetryCancel);
+                   if (result == DialogResult.Cancel) this.Close();
+               }
+               else
+               {
+                   Logic l = new Logic();
+                   var ext = new List<string> { ".doc", ".docx" };
+                   var myFiles = Directory.GetFiles(txtPath.Text).Where(s => ext.Any(opa => s.EndsWith(opa)));
+
+                   Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application { Visible = false };
+                   foreach (var file in myFiles)
+                   {
+
+                       object fileName = file;
+                       Document aDoc = wordApp.Documents.Open(fileName, ReadOnly: false, Visible: false);
+                       aDoc.Activate();
+                       l.FindAndReplace(wordApp, txtFind.Text, txtReplace.Text);
+                       aDoc.Save();
+                       aDoc.Close();
+
+
+                   }
+                   wordApp.Quit();
+                   MessageBox.Show("Uspešna izmena!");
+               }
+           }
+            catch (COMException ex)
+            {
+
+                throw ex;
+            }
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExtract_Click(object sender, EventArgs e)
+        {
+            string kosaCrta = @"\";
+            var ext = new List<string> { ".doc", ".docx" };
+            var myFiles = Directory.GetFiles(txtSourceJob.Text).Where(s => ext.Any(opa => s.EndsWith(opa)));
+
+            Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application { Visible = false };
+            foreach (var file in myFiles)
+            {
+                string fileName1 = Path.GetFileNameWithoutExtension(file);
+                object fileName = file;
+                if (!file.Contains("~"))
+                {
+                    Document aDoc = wordApp.Documents.Open(fileName, ReadOnly: false, Visible: false);
+                    //aDoc.Activate();
+                    if (file.Contains("eng"))
+                    {
+                        foreach (Table tb in aDoc.Tables)
+                        {
+                            var cell1 = tb.Cell(8, 2);
+                            var cell2 = tb.Cell(9, 1);
+                            var cell3 = tb.Cell(9, 2);
+                            string text1 = cell1.Range.Text.Replace("\r\a", string.Empty);
+                            string text2 = cell2.Range.Text.Replace("\r\a", string.Empty);
+                            string text3 = cell3.Range.Text.Replace("\r\a", string.Empty);
+                            string[] tokens1 = text1.Split(Environment.NewLine.ToCharArray());
+                            string[] tokens2 = text2.Split(Environment.NewLine.ToCharArray());
+                            string[] tokens3 = text3.Split(Environment.NewLine.ToCharArray());
+                            for (int i = 0; i < tokens1.Length - 1; i++)
+                            {
+                                //string a3a=tokens1[i].Insert(0,"* ");
+                                richTextBox1.AppendText(tokens1[i].Insert(0, "* "));
+                                richTextBox1.AppendText(Environment.NewLine);
+                                //MessageBox.Show(v);
+                                //test1.Add(v);
+                            }
+                            richTextBox1.AppendText(tokens2[0].Insert(0, "* "));
+                            richTextBox1.AppendText(Environment.NewLine);
+                            for (int i3 = 0; i3 < tokens3.Length - 1; i3++)
+                            {
+                                richTextBox1.AppendText(tokens3[i3].Insert(0, "* "));
+                                richTextBox1.AppendText(Environment.NewLine);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        foreach (Table tb in aDoc.Tables)
+                        {
+                            var cell1 = tb.Cell(6, 2);
+                            var cell2 = tb.Cell(7, 1);
+                            var cell3 = tb.Cell(7, 2);
+                            string text1 = cell1.Range.Text.Replace("\r\a", string.Empty);
+                            string text2 = cell2.Range.Text.Replace("\r\a", string.Empty);
+                            string text3 = cell3.Range.Text.Replace("\r\a", string.Empty);
+                            string[] tokens1 = text1.Split(Environment.NewLine.ToCharArray());
+                            string[] tokens2 = text2.Split(Environment.NewLine.ToCharArray());
+                            string[] tokens3 = text3.Split(Environment.NewLine.ToCharArray());
+                            for (int i = 0; i < tokens1.Length - 1; i++)
+                            {
+                                //string a3a=tokens1[i].Insert(0,"* ");
+                                richTextBox1.AppendText(tokens1[i].Insert(0, "* "));
+                                richTextBox1.AppendText(Environment.NewLine);
+                                //MessageBox.Show(v);
+                                //test1.Add(v);
+                            }
+                            richTextBox1.AppendText(tokens2[0].Insert(0, "* "));
+                            richTextBox1.AppendText(Environment.NewLine);
+                            for (int i3 = 0; i3 < tokens3.Length - 1; i3++)
+                            {
+                                richTextBox1.AppendText(tokens3[i3].Insert(0, "* "));
+                                richTextBox1.AppendText(Environment.NewLine);
+                            }
+                        }
+                    }
+
+                    System.IO.File.WriteAllText(@"" + txtDestinationJob.Text + kosaCrta + "" + fileName1 + ".txt", richTextBox1.Text, Encoding.ASCII);
+                    //System.Diagnostics.Process.Start(@"D:\text.txt");
+                    aDoc.Close();
+                }
+               
+               
+            }
+           
+            wordApp.Quit();
+            MessageBox.Show("Uspešno ste kreirali opis(e)!");
+        }
+        public string Generate(string name)
+        {
+
+
+            return null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            DialogResult result = fbd.ShowDialog();
+
+            if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                txtSourceJob.Text = fbd.SelectedPath;
+                txtDestinationJob.Text = fbd.SelectedPath;
+                string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+                MessageBox.Show("Pronađeno fajlova: " + files.Length.ToString(), "Message");
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            DialogResult result = fbd.ShowDialog();
+
+            if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                txtDestinationJob.Text = fbd.SelectedPath;
+                //string[] files = Directory.GetFiles(fbd.SelectedPath);
+            }
+        }
+    }
+}
